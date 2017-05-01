32889c245ecc8b1d384e73e06657f12fe21381f3
Julio Olivera <julio.olvr@gmail.com>
2017-05-01 18:49:37 -0300

    Add id to participants

--- server/src/models/Participant.ts
+++ server/src/models/Participant.ts
@@ -1,9 +1,15 @@
+import * as uuid from 'uuid/v4'
+
 import IParticipant from '../../../common/interfaces/IParticipant'
 
 class Participant implements IParticipant {
+  public id: string
+
   constructor(
     public name: string
-  ) {}
+  ) {
+    this.id = uuid()
+  }
 
   static fromResult(result): Participant {
     return new Participant(result.name)
